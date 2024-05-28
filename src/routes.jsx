fad064c09ab2e0669d91ce616f1d982110c9aa5e
Gabriel <lopes.biel2588@gmail.com>
2024-05-28 14:18:15 -0300

    Update

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,5 @@
 import SingIn from "../src/Home/Home.jsx";
 import SingUp from "../src/Home copy/Home.jsx";
-import Home from "./Page/Home/Home";
-import Agendamento from "./Page/Agendamento/Agendamento.jsx"
-import Historico from "./Page/Historico/Historico.jsx"
-import { PrivateRoute } from "./Componentes/PrivateRote/PrivateRote";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
@@ -12,11 +8,6 @@ function MainRoutes() {
     <Routes>
       <Route path="/" element={<SingIn />} />
       <Route path="/singup" element={<SingUp />} />
-      <Route path="/" element={<PrivateRoute />}>
-        <Route path="/home" element={<Home />} />
-        <Route path="/agenda" element={<Agendamento />} />
-        <Route path="/historico" element={<Historico />} />
-      </Route>
     </Routes>
   );
 }
